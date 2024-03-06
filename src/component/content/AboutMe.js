cc43e79e4fc5855743c3d6dd4c2dfe88698bd7dd
vongola112 <satrio.handoyo@yahoo.com>
2024-03-06 06:57:14 +0100

    update contentwrapper

--- src/component/content/AboutMe.js
+++ src/component/content/AboutMe.js
@@ -4,20 +4,23 @@ import "./AboutMe.css";
 const AboutMe = () => {
   return (
     <div className="about-me-wrapper">
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Quis viverra nibh
-        cras pulvinar mattis nunc. Et tortor at risus viverra adipiscing. Congue
-        eu consequat ac felis donec. Et tortor consequat id porta nibh. Nullam
-        eget felis eget nunc lobortis. Ipsum suspendisse ultrices gravida dictum
-        fusce ut placerat orci nulla. Tellus in metus vulputate eu scelerisque
-        felis imperdiet proin fermentum. Eros in cursus turpis massa tincidunt
-        dui ut ornare lectus. Tellus at urna condimentum mattis pellentesque id.
-        Risus commodo viverra maecenas accumsan lacus. Congue quisque egestas
-        diam in arcu cursus euismod quis. Dui ut ornare lectus sit amet est. Id
-        semper risus in hendrerit. Ligula ullamcorper malesuada proin libero
-        nunc consequat interdum varius. Dolor sed viverra ipsum nunc.
-      </p>
+      <div className="about-me-intro">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis
+          viverra nibh cras pulvinar mattis nunc. Et tortor at risus viverra
+          adipiscing. Congue eu consequat ac felis donec. Et tortor consequat id
+          porta nibh. Nullam eget felis eget nunc lobortis. Ipsum suspendisse
+          ultrices gravida dictum fusce ut placerat orci nulla. Tellus in metus
+          vulputate eu scelerisque felis imperdiet proin fermentum. Eros in
+          cursus turpis massa tincidunt dui ut ornare lectus. Tellus at urna
+          condimentum mattis pellentesque id. Risus commodo viverra maecenas
+          accumsan lacus. Congue quisque egestas diam in arcu cursus euismod
+          quis. Dui ut ornare lectus sit amet est. Id semper risus in hendrerit.
+          Ligula ullamcorper malesuada proin libero nunc consequat interdum
+          varius. Dolor sed viverra ipsum nunc.
+        </p>
+      </div>
       <div className="about-me-image-wrapper">
         <p>insert picture here</p>
       </div>
