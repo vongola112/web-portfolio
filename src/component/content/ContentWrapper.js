79e71a4375110b3f422903c4881c310bca8d2f5f
vongola112 <satrio.handoyo@yahoo.com>
2025-11-06 17:34:57 +0100

    latest commit

--- src/component/content/ContentWrapper.js
+++ src/component/content/ContentWrapper.js
@@ -3,12 +3,14 @@ import "./ContentWrapper.css";
 import AboutMe from "./AboutMe.js";
 import Portraits from "./Portraits.js";
 import Travel from "./Travel.js";
+import Home from  "./Home.js";
 
 const ContentWrapper = ({ selectedComponent }) => {
   let component;
 
   switch (selectedComponent) {
     case "Home":
+      component = <Home />;
       break;
     case "Portraits":
       component = <Portraits />;
---
+++ src/component/content/Home.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Portraits.css";
+import { useState } from "react";
+import { PhotoAlbum } from "react-photo-album";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+import slides from "./images.js";
+
+const Travel = () => {
+  const [index, setIndex] = useState(-1);
+
+  const images = [];
+
+  for (let index = 0; index < slides.length; index++) {
+    images.push(slides[index]);
+  }
+  const handleClose = () => setIndex(-1);
+  const handleClick = (index) => {
+    setIndex(index);
+  };
+
+  return (
+    <div className="photo-album-container">
+      <PhotoAlbum
+        layout="masonry"
+        photos={images}
+        targetRowHeight={150}
+        onClick={({ index: current }) => handleClick(current)}
+      />
+
+      <Lightbox
+        index={index}
+        slides={images}
+        open={index >= 0}
+        close={handleClose}
+      />
+    </div>
+  );
+};
+
+export default Travel;
